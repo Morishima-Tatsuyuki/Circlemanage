b5c6893f8090b6bd06fa0800115fc6e1a5979695
minakai2722_dot <minakai2722@gmail.com>
2026-04-12 22:52:03 +0900

    CORS設定を追加

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://haisha-app.vercel.app"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
